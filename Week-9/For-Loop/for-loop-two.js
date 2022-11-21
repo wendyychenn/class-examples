5362263c5e928fc04e2a9c9c0d7bfff969f61966
wendyychenn <113089770+wendyychenn@users.noreply.github.com>
2022-11-21 10:10:46 -0800

    Add files via upload

---
+++ Week-9/For-Loop/for-loop-two.js
@@ -0,0 +1,15 @@
+var text = "";
+var total = 0;
+
+for (var i = 0; i < 6; i++) {
+
+    if (i == 3) {
+        text = "finished";
+        break;
+    } else if (i == 5) {
+        i++;
+    }
+    total += i;
+}
+
+console.log(text + " at the number " + i + " with a total of " + total);
---
+++ Week-9/For-Loop/for-of-loops.js
@@ -0,0 +1,9 @@
+var weathers = ["sunny", "rainy", "cloudy"];
+var words = "";
+
+for (var weather of weathers) {
+    words += weather + "\n";
+}
+
+console.log(words);
+
